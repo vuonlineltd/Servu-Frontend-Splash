31a816340057b35f84d30ac04a87b7c8b3d802f7
Laurence Pettitt <laurencepettitt@gmail.com>
2016-08-11 11:09:02 +0100

    Makes links absolute

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title>This is Servu.</title>
-    <link rel="stylesheet" href="style.css" media="screen" title="style" charset="utf-8">
+    <link rel="stylesheet" href="/style.css" media="screen" title="style" charset="utf-8">
   </head>
   <body>
     <h1 id="h1">This is Servu.</h1>
     <p>&copy; <?php echo date("Y"); ?> VuOnline Ltd.</p>
-    <script type="text/javascript" src="splash.js" charset="utf-8"></script>
+    <script type="text/javascript" src="/splash.js" charset="utf-8"></script>
   </body>
 </html>
